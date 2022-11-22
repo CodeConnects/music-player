8283ef4dd9ef1e561087dd2f97431e788fb439f5
oca <codeconnects@gmail.com>
2022-11-21 21:30:20 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-3xl bg-blue-500 text-white p-5">Music Player</h1>
+    </div>
+  );
+}
+
+export default App;
