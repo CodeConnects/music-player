82e83a186acb9a9af9e518589ac68f84e0e897fa
CodeConnects <codeconnects@gmail.com>
2023-06-08 11:34:55 -0400

    add Mongoose db basics

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL);
 
 const connection = mongoose.connection;
 
-connection.on("connected", () => {
-  console.log("MongoDB is connected");
+connection.on('connected', () => {
+  console.log('Mongoose connected successfully');
 });
 
-connection.on("error", (err) => {
-  console.log(err);
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
 });
 
 module.exports = connection;
