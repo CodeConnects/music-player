070bdc33d1e56c5ca37ddb9ba6e1d9a749d09a48
CodeConnects <codeconnects@gmail.com>
2023-06-08 09:51:17 -0400

    add Tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "primary": "#F15412",
-        "secondary": "#1B2430",
-        "active": "#057305",
-      }
-    },
-    plugins: [],
-  }
+    extend: {},
+  },
+  plugins: [],
 }
