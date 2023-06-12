4618a9f2307d66f7ef8ed9eebcbebe7c7e929af6
CodeConnects <codeconnects@gmail.com>
2023-06-12 09:11:36 -0400

    register form layout

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="flex min-h-screen items-start justify-center">
+      <div>
+        <input type="text" placeholder="Username" className="border-2 border-gray-300 rounded-md p-2 m-2" />
+        <input type="text" placeholder="Email" className="border-2 border-gray-300 rounded-md p-2 m-2" />
+        <input type="password" placeholder="Password" className="border-2 border-gray-300 rounded-md p-2 m-2" />
+        <input type="password" placeholder="Confirm Password" className="border-2 border-gray-300 rounded-md p-2 m-2" />
+        <button className="border-2 border-gray-300 rounded-md p-2 m-2">Register</button>
+      </div>
+    </div>
   );
 }
 
