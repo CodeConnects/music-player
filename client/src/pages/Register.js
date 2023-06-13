644e4deb422a9eaa30c329f60dedf2c0bf504d14
CodeConnects <codeconnects@gmail.com>
2023-06-13 09:18:58 -0400

    fix register form state bugs

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const register = () => {
+    console.log(user);
+  }
   return (
-    <div className="flex min-h-screen items-start justify-center">
-      <div>
-        <input type="text" placeholder="Username" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <input type="text" placeholder="Email" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <input type="password" placeholder="Password" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <input type="password" placeholder="Confirm Password" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <button className="border-2 border-gray-300 rounded-md p-2 m-2">Register</button>
+    <div className="flex min-h-screen items-start justify-center text-center ">
+      <div className="flex flex-col p-5 mt-6 border border-gray-400">
+        
+        <h1 className="text-3xl font-bold mb-5 mt-1">Register</h1>
+        
+        <input 
+          type="text" 
+          placeholder="Username" 
+          value={user.username} 
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          className="text-center border-2 border-gray-300 rounded-md p-2 m-2"
+        />
+        <input 
+          type="text" 
+          placeholder="Email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          className="text-center border-2 border-gray-300 rounded-md p-2 m-2"
+        />
+        <input 
+          type="password" 
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          className="text-center border-2 border-gray-300 rounded-md p-2 m-2"
+        />
+        <input 
+          type="password" 
+          placeholder="Confirm Password"
+          value={user.confirmPassword}
+          onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })}
+          className="text-center border-2 border-gray-300 rounded-md p-2 m-2"
+        />
+        
+        <button
+          onClick={register} className="border-2 border-gray-300 rounded-md p-2 m-2 bg-sky-300">Register</button>
       </div>
     </div>
   );
