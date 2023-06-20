e3d82a8bd6ee12fe69f8174e1e3dcbf984732391
CodeConnects <codeconnects@gmail.com>
2023-06-20 09:41:39 -0400

    put User route API in express server

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
+
+app.use(express.json());
+const userRoute = require('./routes/userRoute');
+app.use('/api/users', userRoute);
+
 const port = 5000;
 app.listen(port, () => console.log(`Nodejs server is running on port ${port}!`));
