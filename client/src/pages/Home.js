e133a5db384cc2a48daa6f83dd52293bcecfb813
CodeConnects <codeconnects@gmail.com>
2023-08-09 08:53:39 -0400

    add home page authorization

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+  const [userData, setUserData] = useState(null);
+  const getUserData = async () => {
+    try {
+      const response = await axios.post("/api/users/get-user-data", {}, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.data.success) {
+        setUserData(response.data.data);
+      } else {
+        alert(response.data.message);
+      }
+      //console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">{userData?.userName}</h1>
+    </div>
   );
 }
 
