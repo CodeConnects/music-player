7d4d8f877916dda5397f85d8784787da231d8df5
CodeConnects <codeconnects@gmail.com>
2023-08-11 16:51:18 -0400

    add path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
-
+const path = require('path');
 app.use(express.json());
 const userRoute = require('./routes/userRoute');
 app.use('/api/users', userRoute);
 
 const port = 5000;
-app.listen(port, () => console.log(`Nodejs server is running on port ${port}!`));
+app.listen(port, () => console.log(`Nodejs server port ${port}!`));
