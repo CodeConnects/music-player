b635907feeda8a8ad5edff0830243b0a1a694e15
CodeConnects <codeconnects@gmail.com>
2023-08-11 16:28:47 -0400

    ProtectedRoute boilerplate

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProtectedRoute({children}) {
+  const [readyToRender, setReadyToRender] = React.useState(false);
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ProtectedRoute;
