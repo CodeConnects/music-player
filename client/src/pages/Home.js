da5e3f53844b153861f36ac939867a72cdc6388d
CodeConnects <codeconnects@gmail.com>
2023-08-16 18:28:36 -0400

    add Link and show username

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [userData, setUserData] = useState(null);
+  
   const getUserData = async () => {
     try {
       const response = await axios.post("/api/users/get-user-data", {}, {
@@ -12,22 +14,31 @@ function Home() {
       });
       if (response.data.success) {
         setUserData(response.data.data);
-      } else {
+        console.log(response.data.data);
+      }
+      else {
         alert(response.data.message);
       }
-      //console.log(response.data);
+      console.log(response.data);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getUserData();
+    if (userData === null) {
+      getUserData();
+    }
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">{userData?.userName}</h1>
+    <div className="flex justify-center p-10">
+      <div className="text-center form-wrap flex flex-col p-10 mt-6 border border-gray-400">
+        <h1 className="text-4xl font-bold p-4">Hello {userData?.username}</h1>
+        <hr className="p-2" />
+        <Link to="/register" className="text-gray-600 underline p-2">Register</Link>
+        <Link to="/login" className="text-gray-600 underline">Login</Link>
+      </div>
     </div>
   );
 }
