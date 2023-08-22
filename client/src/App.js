6a08dfd6bbe5bc17bb03df66ceee9ddccc5fee91
CodeConnects <codeconnects@gmail.com>
2023-08-22 09:29:18 -0400

    wrap registration screens in PublicRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<PublicRoute><Login/></PublicRoute>} />
+          <Route path="/register" element={<PublicRoute><Register/></PublicRoute>} />
         </Routes>
       </BrowserRouter>
     </div>
