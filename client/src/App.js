cee39df77f8fa543c7deaf60a2f37480c89c44a2
CodeConnects <codeconnects@gmail.com>
2023-08-30 14:24:58 -0400

    add spinner to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute';
+import Spinner from './components/Spinner';
 
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
+
   return (
     <div className="App">
+      {loading && <Spinner/>}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
