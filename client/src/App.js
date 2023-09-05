2dd3d41a28af1c16e9550705b364b73c6ffe5376
CodeConnects <codeconnects@gmail.com>
2023-09-05 10:48:04 -0400

    add Hot Toast primary component call

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -16,6 +17,15 @@ function App() {
   return (
     <div className="App">
       {loading && <Spinner/>}
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            fontSize: '16px',
+          },
+        }}
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
