0324169d81cea69e536c80cada0cd80696f48f40
CodeConnects <codeconnects@gmail.com>
2023-09-08 09:43:57 -0400

    add Songs api

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const dbConfig = require('./config/dbConfig');
 const path = require('path');
 app.use(express.json());
 const userRoute = require('./routes/userRoute');
+const songsRoute = require('./routes/songsRoute');
+
 app.use('/api/users', userRoute);
+app.use('/api/songs', songsRoute);
 
 const port = 5000;
 app.listen(port, () => console.log(`Nodejs server port ${port}!`));
