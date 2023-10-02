0921050592c38b87ba80979e3f77a3c9a1f2d641
CodeConnects <codeconnects@gmail.com>
2023-10-02 13:26:41 -0400

    hide path module

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
-const path = require('path');
+//const path = require('path');
+
 app.use(express.json());
 const userRoute = require('./routes/userRoute');
 const songsRoute = require('./routes/songsRoute');
